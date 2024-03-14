8336fe8fc80470a7c8ab68caf77b7eeb177c39f0
Gabriel Cruz dos Passos <passosc.gabriel@hotmail.com>
2024-03-14 15:10:13 -0300

    Arquivos do curso de JavaScript

---
+++ Aula55/index.js
@@ -0,0 +1,24 @@
+// IIFE -> Immediately Invoked Function Expression (Função auto-invocada)
+function qualquerCoisa() {
+    console.log(123);
+}
+qualquerCoisa();
+
+(function() {// Função auto-invocada
+    const nome = 'Luiz';
+    console.log(nome);
+})();
+
+const nome = 'Qualquer coisa';
+
+(function(idade, peso, altura){ //parametros
+    const sobrenome = 'Miranda';
+    function criaNome(nome) {
+        return nome + ' ' + sobrenome;
+    }
+    function falaNome() {
+        console.log(criaNome('Luiz'));
+    }
+    falaNome();
+    console.log(idade, peso, altura);
+})(30, 80, 1.80); //argumentos
